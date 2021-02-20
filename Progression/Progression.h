5252ad45b91b10f13a63149dd88e4280a51322a4
Oleg Badretdinov <badretdinov@me.com>
2021-02-20 15:16:22 +0400

    Initial Commit

---
+++ Progression/Progression.h
@@ -0,0 +1,18 @@
+//
+//  Progression.h
+//  Progression
+//
+//  Created by Oleg Badretdinov on 20.02.2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Progression.
+FOUNDATION_EXPORT double ProgressionVersionNumber;
+
+//! Project version string for Progression.
+FOUNDATION_EXPORT const unsigned char ProgressionVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Progression/PublicHeader.h>
+
+
